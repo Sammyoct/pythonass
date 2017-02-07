b518d37b19e335f47f0e37b0eff63fe9c79c3416
Sameer Bhosekar <sammyoct@gmail.com>
2017-02-07 13:01:49 +0530

    12-histogram

---
+++ 12.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+def generate_x_of_chars(a):
+    j = []
+    for x in list(range(int(a))):
+        j.append("*") 
+
+    j = "".join(str(x) for x in j)
+    print j 
+
+def get_input():
+    a = raw_input("Please enter the number to repeat character:")
+    print "you have enter numember:",a
+    return a
+
+def get_start():
+    j = []
+    s = raw_input("end of inputs with 'n'")
+    while s != 'n':
+        s = get_input()
+        if s != 'n':
+            j.append(s)
+
+    for x in j:
+        generate_x_of_chars(x)
+
+get_start()
